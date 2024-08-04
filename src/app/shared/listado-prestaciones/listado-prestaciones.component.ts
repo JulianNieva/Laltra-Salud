eaf6e737279f09fc624a9b9ac3db94607dc2bbf5
JulianNieva <juliannieva90@gmail.com>
2024-08-04 17:30:58 -0300

    Feature: Agregado listado de prestaciones

---
+++ src/app/shared/listado-prestaciones/listado-prestaciones.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-listado-prestaciones',
+  templateUrl: './listado-prestaciones.component.html',
+  styleUrl: './listado-prestaciones.component.scss'
+})
+export class ListadoPrestacionesComponent {
+
+}
